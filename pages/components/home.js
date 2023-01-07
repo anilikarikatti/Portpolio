7d0f510e18ea7758bded6ad2fa30809c0a65d899
anilikarikatti <anilkarikatti333@gmail.com>
2023-01-07 17:17:55 +0530

    response started

--- pages/components/home.js
+++ pages/components/home.js
@@ -4,12 +4,12 @@ export default function Home(){
     return(
         <>
         <Main1></Main1>
-        <div className='about' >
-            <div className='img backimg'>
+        <div className='about flex sm:flex-row  flex-col sm:h-screen h-fit' >
+            <div className='sm:h-full sm:w-full  backimg bg-cover  h-96 w-96 sm:ml-0 ml-20 '>
                 
 
             </div>
-            <div className='img'>
+            <div className='sm:h-full sm:w-full  '>
             {/* Hello,This is
 
 Irayya Hiremath
@@ -20,12 +20,12 @@ I am looking for an opportunity in an esteemed organisation to put in my best ef
 I Have done projects using following WebTechnologies */}
 
 
-                <h1 className="color-white">Hello , This is </h1>
-                <h1 className="">Anil I Karikatti</h1>
-                <h3 className="line-space">I am a Software Engineer  I love coding , development and make new web experiences for people.
-                    I am looking for an opportunity in an esteemed organisation to put in my best efforts and always hunger for learning new things and put those skills into continuous implementation to achieve personal growth and fulfil organisational goals.
+                <h1 className="text-black-100 sm:mt-40 mt-8 sm:ml-0 ml-40 ">Hello , This is </h1>
+                <p className="sm:text-8xl sm:ml-0 ml-40">Anil I Karikatti</p>
+                <h3 className="line-space sm:mt-10 mt-3 sm:ml-0 ml-10">I am a Software Engineer  I love coding , development and make new web experiences for people.
+                    I am looking for an opportunity in an esteemed organisation to put in my best efforts and always hunger for learning new things and put those skills into continuous implementation to achieve personal growth and fulfill organisational goals.
                     </h3>
-                    <h3 className="margin-top-10">
+                    <h3 className="margin-top-10 am:ml-0 ml-10">
                     I Have done projects using following WebTechnologies</h3>
 
                     <div className="tech">
