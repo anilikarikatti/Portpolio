ecbd5b20e3dcc855196c85048658374a092e4366
anilikarikatti <anilkarikatti333@gmail.com>
2023-02-20 17:54:23 +0530

    contacts

--- pages/components/contact.js
+++ pages/components/contact.js
@@ -1,13 +1,19 @@
 import { Main1 } from "../index"
 
+
 export default function Contact(){
     return(
         <>
-
+                <div className='mt-20 text-center' >
+                <div>
                 <Main1></Main1>
-                <div className='about' >
 
-                                                        
+                </div>
+                    <div>
+                        <h3 className="capitalize">get in touch</h3>
+                        
+                    </div>
+
             </div>
 
         </>
